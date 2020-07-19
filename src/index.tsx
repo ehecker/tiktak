e05381d49d1cfefab00968fa01117bf1b09b3916
ehecker <ezhecker@gmail.com>
2020-07-18 18:40:36 -0700

    Clean code & finalize project

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,29 @@ import React, {ReactNode, useState} from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
 
-
-
 type SquareValues = "X" | "O" | null
+
+const calculateWinner = (squares: SquareValues[]):SquareValues => {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i]
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a]
+        }
+    }
+
+    return null
+}
 
 // SQUARE COMPONENT ==============================================
 interface SquareProps {
@@ -133,25 +153,3 @@ const Game: React.FC = () => {
   
 ReactDOM.render(<Game />, document.getElementById("root"))
 
-const calculateWinner = (squares: SquareValues[]):SquareValues => {
-    const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ]
-
-    for (let i = 0; i < lines.length; i++) {
-        const [a, b, c] = lines[i]
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a]
-        }
-    }
-
-    return null
-}
-  
